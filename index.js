e4cc9e92083b717b4b572e4e77e9cf978d28ea47
AmanRajSharma001 <amanrajsharma699@gmail.com>
2026-03-30 00:50:43 +0530

    opening animations

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ let intro = document.getElementById("intro")
 let title = document.getElementById("title");
 let press = document.getElementById("press");
 let openingvideo = document.getElementById("bg-video")
-const maincontent = document.getElementById("main-content")
-
+let entercount = 0;
 let i = 0;
 function Type(){
     if (i<txt.length){
         title.innerHTML+=txt[i];
         i++
-        time = 
-        setTimeout(Type,100)
+        time = setTimeout(Type,100)
     }
   }
 Type();
@@ -24,32 +22,12 @@ function remove() {
   }
 }
 
-  
-const songs = ["audio/audio1.mp3","audio/audio2.mp3","audio/audio3.mp3","audio/audio4.mp3"];
-
-let currentSong = 0;
-
-const audio = document.getElementById("background");
-
-function Play(index) {
-  audio.src = songs[index];
-  audio.play();
-}
-
-audio.addEventListener("ended", () => {
-  currentSong = (currentSong + 1) % songs.length;
-  Play(currentSong);
-});
-
-document.addEventListener("click", () => {
-  Play(currentSong);
-}, { once: true });
-
-
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
+  if (e.key === "Enter" && entercount == 0) {
+    entercount++
     const enter = document.getElementById("enter");
     enter.muted = false;
+    enter.play()
     const press = document.getElementById("press");
     press.removeAttribute("id");
     press.classList.add("fastblink");
@@ -59,12 +37,17 @@ document.addEventListener("keydown", (e) => {
       circle.classList.add("expand");
     },1000)
     setTimeout(()=>{
-      openingvideo.classList.add('hide')
-      intro.classList.add("hide")
-      circle.classList.add("hide")
-      maincontent.style.display = "flex";
+      intro.classList.add("fade-out");
+      setTimeout(() => {
+        intro.style.display = "none";
+        document.getElementById("circle").style.display = "none";
+        Play(currentSong);
+      }, 500);
     },1500)
-    audio.pause();
     remove()
   }
+  setTimeout(() => {
+      localStorage.setItem("playMusic", "true");
+      window.location.href = "main.html";
+    }, 1500);
 });
